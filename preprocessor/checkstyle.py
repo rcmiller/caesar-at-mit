3988be9a6b0e02693df75ab39638936e80cc1714
Rob Miller <rcm@mit.edu>
2019-04-08 15:00:16 -0400

    now using checkstyle 8.18; preprocessor now summarizes its checkstyle comments

--- preprocessor/checkstyle.py
+++ preprocessor/checkstyle.py
@@ -2,16 +2,19 @@ from review.models import Submission, File, Chunk, Batch, Comment
 from django.contrib.auth.models import User
 from xml.dom.minidom import parseString
 from subprocess import Popen, PIPE
-from collections import defaultdict
+from collections import defaultdict, Counter
 import re, sys
 
 checkstyle_settings = {
     'settings': '/var/django/caesar/preprocessor/checks.xml',
-    'jar': '/var/django/caesar/preprocessor/checkstyle-5.9-all.jar',
+    'jar': '/var/django/caesar/preprocessor/checkstyle.jar',
     }
 
 def generate_checkstyle_comments(code_objects, save, batch, suppress_comment_regexes):
   checkstyle_user,created = User.objects.get_or_create(username='checkstyle')
+
+  module_histogram = Counter() # checkstyle module name => # of complaints it made
+  message_example = {} # module name => example of a complaint
 
   for (submission, files, chunks) in code_objects:
     chunkMap = dict([(chunk.file.path, chunk) for chunk in chunks if chunk.student_lines > 0])
@@ -43,6 +46,8 @@ def generate_checkstyle_comments(code_objects, save, batch, suppress_comment_reg
         message = commentNode.getAttribute('message')
         line = commentNode.getAttribute('line')
         checkstyleModule = commentNode.getAttribute('source')
+        module_histogram[checkstyleModule] += 1
+        message_example[checkstyleModule] = message
         comment = Comment(
           type='S',
           text=message,
@@ -54,3 +59,7 @@ def generate_checkstyle_comments(code_objects, save, batch, suppress_comment_reg
         if save:
           comment.save()
       print("checkstyle: on", chunk.name, 'I made', len(commentNodes), 'comments')
+  
+  print("overall checkstyle message frequencies:")
+  for module, count in module_histogram.most_common():
+    print(count, module.split('.')[-1], "e.g.", message_example[module])
