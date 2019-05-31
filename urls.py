5597b5ed7d9bf4e1b4bd2f486a917e7d93698755
Rob Miller <rcm@mit.edu>
2019-05-31 12:24:21 -0400

    now merged into review/urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-import review.urls
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include(review.urls)),
-]
