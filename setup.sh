32b5e6141a8069e93856151d52e13c98f5a50c12
Rob Miller <rcm@mit.edu>
2019-07-02 10:35:59 -0400

    apt installs should now *really* be noninteractive

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# fixes https://bugs.launchpad.net/ubuntu/+source/eglibc/+bug/935681
-export DEBIAN_FRONTEND=noninteractive
-
 CAESAR_DIR=/var/django/caesar
 
 # Set time zone.
@@ -11,25 +8,25 @@ CAESAR_DIR=/var/django/caesar
 #/usr/sbin/dpkg-reconfigure tzdata
 
 # This part is needed by OpenStack VM:
-# display progress of Ubuntu unattended-upgrade
-# (since it is typically running right after the VM instance is launched)
-# https://askubuntu.com/questions/934807/unattended-upgrades-status
-tail -f /var/log/unattended-upgrades/unattended-upgrades.log &
-
-# and wait for unattended-upgrades to finish
 # https://unix.stackexchange.com/questions/463498/terminate-and-disable-remove-unattended-upgrade-before-command-returns
+echo waiting for unattended upgrades to finish
 sudo systemd-run --property="After=apt-daily.service apt-daily-upgrade.service" --wait /bin/true
 
 # Install Linux packages we need.
-sudo apt-get update
+# The combination of DEBIAN_FRONTEND=noninteractive and -y ensure that we say Yes
+# to every prompt, rather than stopping to prompt for user input.
+sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
+
 #sudo apt-get upgrade
-sudo apt-get install -y apache2 apache2-dev libapache2-mod-wsgi-py3 # for Apache
-sudo apt-get install -y sqlite3 # for development
-sudo apt-get install -y libmysqlclient-dev libldap2-dev libsasl2-dev # MySQL, LDAP
-sudo apt-get install -y default-jre # in order to run checkstyle
-sudo apt-get install -y python3.7 python3-pip # Django 2.1+ requires Python 3.7+
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y apache2 apache2-dev libapache2-mod-wsgi-py3 # for Apache
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y sqlite3 # for development
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y libmysqlclient-dev libldap2-dev libsasl2-dev # MySQL, LDAP
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y default-jre # in order to run checkstyle
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3.7 python3-pip # Django 2.1+ requires Python 3.7+
 
 # Install Python packages we need.
+# sudo -H ensures that pip3 uses root's home folder for its cache, rather than
+# filling ubuntu's home folder cache with root-owned files.
 cd /tmp
 sudo -H pip3 install -r $CAESAR_DIR/requirements.txt
 
